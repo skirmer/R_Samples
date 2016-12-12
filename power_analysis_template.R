9ecb909f270f09d227d9b22ecb2187ab50c4df77
Stephanie Kirmer <skirmer@users.noreply.github.com>
2016-12-11 20:22:38 -0600

    Update power_analysis_template.R

--- power_analysis_template.R
+++ power_analysis_template.R
@@ -33,44 +33,19 @@ library(reshape)
 #Create the outcomes if you haven't yet - looking for a binary variable here.
 
 #check the SD to make sure thresholds are right for your subpopulations below
-sd(dataset$act, na.rm=T)
-sd(dataset$gpa_12th, na.rm=T)
+sd(dataset$test, na.rm=T)
+sd(dataset$gpa, na.rm=T)
 
 #Create the different sub-populations- choose the one you need in a given moment, don't run both or you'll just get the last one.
-population <- filter(dataset, act > (20-4.5), act < (21+4.5), gpa_12th > (3-.75), gpa_12th < (3+.75),  toupper(STUDENT_RACE)=="HISPANIC")
-population <- filter(dataset, act > (20-4.5), act < (20+4.5), gpa_12th > (3-.75), gpa_12th < (3+.75))
+population <- filter(dataset, test > (20-4.5), test < (21+4.5), gpa > (3-.75), gpa < (3+.75))
 
-#anycollege
-anycol <- prop.table(table(population$went_college_flag,population$STUDENT_FOODSERVICE_INDICATOR, useNA="ifany", dnn=c("college", "frl")),2)
-anycol_n <- table(population$STUDENT_FOODSERVICE_INDICATOR, useNA="ifany")
-anycol_frl <- rbind(anycol[2,], anycol_n)
-
-anycol <- prop.table(table(population$went_college_flag,population$gender, useNA="ifany", dnn=c("college", "gend")),2)
-anycol_n <- table(population$opi_gender, useNA="ifany")
-anycol_gend <- rbind(anycol[2,], anycol_n)
-
-#graduation
-population <- filter(dataset, act > (20-4.5), act < (21+4.5), gpa_12th > (3-.75), gpa_12th < (3+.75),  toupper(STUDENT_RACE)=="HISPANIC", cohort=="2008")
-population <- filter(dataset, act > (20-4.5), act < (20+4.5), cohort=="2008", gpa_12th > (3-.75), gpa_12th < (3+.75))
-
-graduation <- prop.table(table(population$GRADUATED,population$STUDENT_FOODSERVICE_INDICATOR, useNA="ifany", dnn=c("grad", "frl")),2)
-graduation_n <- table(population$STUDENT_FOODSERVICE_INDICATOR, useNA="ifany")
-graduation_frl <- rbind(graduation[2,], graduation_n)
-
-graduation <- prop.table(table(population$GRADUATED,population$gender, useNA="ifany", dnn=c("grad", "gend")),2)
-graduation_n <- table(population$gender, useNA="ifany")
-graduation_gend <- rbind(graduation[2,], graduation_n)
-
+#trait1
+t1 <- prop.table(table(population$trait1,population$trait2, useNA="ifany", dnn=c("t1", "t2")),2)
+t1_n <- table(population$trait2, useNA="ifany")
+t1_t2 <- rbind(t1[2,], t1_n)
 
 #Look at the results
-rownames(anycol_frl) <- c("coll_prob", "n")
-rownames(graduation_frl) <- c("grad_prob", "gr_n")
-rbind(anycol_frl,graduation_frl)
-
-rownames(anycol_gend) <- c("coll_prob", "n")
-rownames(graduation_gend) <- c("grad_prob", "gr_n")
-rbind(anycol_gend, graduation_gend)
-
+rownames(t1_t2) <- c("t1_prob", "n")
 
 # Run the power analysis, inputting different variations of the results - you can use the function help section to decide what you want to do there.
 power.prop.test(n=150, p1=.264, p2=NULL, sig.level=.05, power=.8)
